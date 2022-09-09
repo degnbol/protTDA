3624253bacbaf5fa9bfdcb3c12ce0df659e320ea
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-09-09 16:54:10 +1000

    pdb to xyz scripts

---
+++ src/pdb2tsv.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+# USE: pdb2tsv.sh < INFILE.pdb > OUTFILE.tsv
+# Source: https://www.wwpdb.org/documentation/file-format-content/format33/sect9.html#ATOM
+TMP=$RAND.tmp
+grep '^ATOM' > $TMP
+paste -d, \
+    <(cut -c07-11 $TMP) \
+    <(cut -c13-16 $TMP) \
+    <(cut -c17-17 $TMP) \
+    <(cut -c18-20 $TMP) \
+    <(cut -c22-22 $TMP) \
+    <(cut -c23-26 $TMP) \
+    <(cut -c27-27 $TMP) \
+    <(cut -c31-38 $TMP) \
+    <(cut -c39-46 $TMP) \
+    <(cut -c47-54 $TMP) \
+    <(cut -c55-60 $TMP) \
+    <(cut -c61-66 $TMP) \
+    <(cut -c77-78 $TMP) \
+    <(cut -c79-80 $TMP) |
+    mlr --c2t --hi label atomi,atom,altLoc,res,chain,resi,iCode,x,y,z,occupancy,temp,element,charge +\
+    clean-whitespace
+
---
+++ src/pdb2xyzCA.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+$0:h/pdb2tsv.sh | mlr -t filter '$atom == "CA"' + cut -f x,y,z,resi
