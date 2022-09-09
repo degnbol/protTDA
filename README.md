81b3af6a88d8d4ede5d977db89249440d78918f1
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-09-09 14:29:09 +1000

    +hyperTDA

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # protTDA
 Topological Data Analysis on protein structures from crystalised structures and alphaFold.
+
+## INSTALL
+Go to tools/hyperTDA and follow its install instructions in its README.
+
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+# make a command "git root" that gives the root folder of the repo.
+git config alias.root 'rev-parse --show-toplevel'
+# make sure submodules of other publications are initialized.
+git submodule update --init
