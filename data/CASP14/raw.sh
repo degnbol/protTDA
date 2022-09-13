596640892473fdcffc350ec6e4d90da3356f19b7
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-09-13 10:46:50 +1000

    target list

--- data/CASP14/raw.sh
+++ data/CASP14/raw.sh
@@ -7,6 +7,7 @@ wget -O T.tar.gz https://predictioncenter.org/download_area/CASP14/targets/casp1
 wget -O T-dom.tar.gz https://predictioncenter.org/download_area/CASP14/targets/casp14.targets.T-dom.public_11.29.2020.tar.gz
 wget -O oligo.tar.gz https://predictioncenter.org/download_area/CASP14/targets/casp14.targets.oligo.public_11.29.2020.tar.gz
 wget -O casp14.faa https://predictioncenter.org/download_area/CASP14/sequences/casp14.seq.txt
+wget -O targets.csv 'https://predictioncenter.org/casp14/targetlist.cgi?type=csv'
 
 # extact
 for file in *.tar.gz; do
