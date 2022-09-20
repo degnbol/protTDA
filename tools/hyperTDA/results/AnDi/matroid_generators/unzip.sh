dd06903a9b00a443cda950b723302999658fa7a1
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-09-20 16:35:56 +1000

    rm hyperTDA subtree so we can use submodule instead

--- tools/hyperTDA/results/AnDi/matroid_generators/unzip.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-for dir in Model_?; do
-    cd $dir
-    unzip H.zip
-    unzip nodeCents.zip
-    cd -
-done
--- tools/hyperTDA/src/AUC.jl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env julia
-using ROC # https://github.com/diegozea/ROC.jl
-# works for both vectors and matrices. 
-AUC(ŷ, y) = ROC.roc(ŷ, y) |> ROC.AUC
