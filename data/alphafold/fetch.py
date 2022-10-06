777e8a799ec1611567db54b45ec139410f7defc5
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-10-06 04:45:22 +0000

    made into functions

--- data/alphafold/fetch.py
+++ data/alphafold/fetch.py
@@ -3,7 +3,6 @@ from google.cloud import storage
 import numpy as np
 import tarfile, io, gzip
 
-
 def cif2xyz(cif):
     """
     - cif: fileobj or other object where each iteration produces a string cif line.
@@ -15,21 +14,22 @@ def cif2xyz(cif):
 client = storage.Client(project="protTDA")
 bucket = client.bucket("public-datasets-deepmind-alphafold")
 
-# version reading a single cif at a time
-for blob in bucket.list_blobs(max_results=1):
-    break # debug
-    if not blob.name.endswith(".cif"): continue
-    # if blob.size > 100000: continue
-    with blob.open() as cif: xyz = cif2xyz(cif)
-    np.save("xyz/" + blob.name.removesuffix(".cif"), xyz)
-    
-# version reading a proteome at a time that may have multiple compressed cifs.
-for blob in bucket.list_blobs(max_results=2, prefix="proteomes"):
-    # break
-    with tarfile.open(fileobj=io.BytesIO(blob.download_as_bytes())) as tar:
-        for member in tar.getmembers():
-            if member.name.endswith(".cif.gz"):
-                with gzip.open(tar.extractfile(member), 'rt') as cif:
-                    xyz = cif2xyz(cif)
+def gen_xyzs(max_results=None, size_limit=None):
+    """ Yield numpy array with columns: x, y, z."""
+    for blob in client.list_blobs(bucket, max_results=max_results):
+        if not blob.name.endswith(".cif"): continue
+        if size_limit is not None and blob.size > size_limit: continue
+        with blob.open() as cif:
+            yield cif2xyz(cif)
+
+def gen_xyzs_proteomes(max_results=None, size_limit=None):
+    """ version reading a proteome at a time that may have multiple compressed cifs. """
+    for blob in client.list_blobs(bucket, max_results=max_results, prefix="proteomes"):
+        if size_limit is not None and blob.size > size_limit: continue
+        with tarfile.open(fileobj=io.BytesIO(blob.download_as_bytes())) as tar:
+            for member in tar.getmembers():
+                if member.name.endswith(".cif.gz"):
+                    with gzip.open(tar.extractfile(member), 'rt') as cif:
+                        yield cif2xyz(cif)
 
 
