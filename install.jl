d836cd99e78f90cafdbd61ac717b091dad2c9cfa
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-10-18 05:47:45 +0000

    install julia pkgs and no python fetch

---
+++ install.jl
@@ -0,0 +1,4 @@
+#!/usr/bin/env julia
+using Pkg
+Pkg.activate(".")
+Pkg.add(["SparseArrays", "LinearAlgebra", "JSON", "GZip", "Ripserer"])
--- install.sh
+++ install.sh
@@ -4,6 +4,3 @@ git config alias.root 'rev-parse --show-toplevel'
 # make sure submodules of other publications are initialized.
 git submodule update --init
 
-pip install --upgrade google-cloud-storage
-# assuming gcloud sdk is installed. login to the project specified in fetch.py
-gcloud auth application-default login
