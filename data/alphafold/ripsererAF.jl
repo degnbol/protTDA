444a6732d924bfe9e0d27e94ff33aace0228f253
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-10-18 10:09:34 +0000

    only add non-trivial centralities to json

--- data/alphafold/ripsererAF.jl
+++ data/alphafold/ripsererAF.jl
@@ -1,33 +1,36 @@
 #!/usr/bin/env julia
-using PyCall
-@pyinclude "fetch.py"
 using Ripserer
 using JSON
 using GZip
-# https://www.lucidchart.com/techblog/2019/12/06/json-compression-alternative-binary-formats-and-compression-methods/
-# using CodecXz
 using SparseArrays
 using LinearAlgebra # norm and normalize etc
+using Random
 
-function blob2PC(blob::PyObject)
+function cif2PC(fname::String)
     PC = Tuple{Float64,Float64,Float64}[]
-    fh = blob.open()
-    for line in fh
-        startswith(line, "ATOM") || continue
-        l = split(line)
-        l[4] == "CA" || continue
-        push!(PC, (parse(Float64,l[11]), parse(Float64,l[12]), parse(Float64,l[13])))
+    GZip.open(fname, "rt") do io
+        lines = readlines(io)
+        lines = lines[startswith.(lines, "ATOM")]
+        for line in lines
+            l = split(line)
+            l[4] == "CA" || continue
+            push!(PC, (parse(Float64,l[11]), parse(Float64,l[12]), parse(Float64,l[13])))
+        end
     end
-    fh.close()
     PC
 end
 
 PC2PH(PC::Vector{Tuple{Float64,Float64,Float64}}) = ripserer(Alpha(PC); dim_max=2, alg=:involuted)
 
-barcodes(PH, dim::Int) = hcat(collect.(collect(PH[dim+1]))...)'
-representatives(PH, dim::Int) = [[collect(r.simplex) for r in collect(c)] for c in representative.(PH[dim+1])]
+function barcodes(PH, dim::Int)
+    # use init for default 0×2 (after adjoint) output for trivial barcodes
+    reduce(hcat, collect.(collect(PH[dim+1])); init=Matrix{Float64}(undef,2,0))'
+end
+function representatives(PH, dim::Int)::Vector{Vector{Vector{Int}}}
+    [[collect(r.simplex) for r in collect(c)] for c in representative.(PH[dim+1])]
+end
 
-function representatives2H(reps::Vector{Vector{Vector{Int}}}, n::Int)
+function rep2H(reps::Vector{Vector{Vector{Int}}}, n::Int)
     Is = [n for    r  in           reps  for e in r for n in e]
     Js = [j for (j,r) in enumerate(reps) for e in r for n in e]
     sparse(Is, Js, true, n, length(reps))
@@ -35,7 +38,9 @@ end
 
 """
 Taken from centrality_tools.jl in repo node-edge-hypergraph-centrality.
-fgϕψs defined according to the max method (see Tudisco et al. 2021)
+fgϕψs defined according to the max method (see Tudisco et al. 2021).
+Modified to only return node centralities since edge centralities were 
+too similar to the input persistences.
 """
 function centralities(B; maxiter::Int=100, tol::Float64=1e-6,
         edge_weights::Vector{Float64}=ones(size(B,2)),
@@ -58,47 +63,56 @@ function centralities(B; maxiter::Int=100, tol::Float64=1e-6,
         x = normalize(sqrt.(x0 .* g(B  * W *   y0 )), 1)
         y = normalize(sqrt.(y0 .* ψ(B' * N * ϕ(x0))), 1)
         
-        norm(x-x0,1) + norm(y-y0,1) < tol && return x, y
+        norm(x-x0,1) + norm(y-y0,1) < tol && return x
         
         x0 = x
         y0 = y
     end
-    x0, y0
+    x0
 end
 
+compl = vcat([readdir(d2) for d1 in readdir("PH"; join=true) for d2 in readdir(d1; join=true)]...)
+# strip extensions
+compl = [f[1:end-8] for f in compl]
+println(length(compl), " completed")
 
-for blob in py"gen_blobs"o()
-    # discard "AF-" ... "-model_v3.cif"
-    name = blob.name[4:end-13]
+cifs = readdir("structures/proteomes/")
+cifs = cifs[endswith.(cifs, ".cif.gz")]
+# strip AF- and -model_v3.cif.gz
+cifs = [f[4:end-16] for f in cifs]
+println(length(cifs), " cifs")
+
+todo = setdiff(cifs, compl) |> shuffle
+println(length(todo), " todo")
+
+for name in todo
     outdir = "PH/"*name[1:2]*"/"*name[3:4]
     outfile = outdir*"/"*name*".json.gz"
     isfile(outfile) && continue
     mkpath(outdir)
     touch(outfile)
-    PC = blob2PC(blob)
+    PC = try cif2PC("structures/proteomes/AF-$name-model_v3.cif.gz")
+    catch
+        println("problem reading structures/proteomes/AF-$name-model_v3.cif.gz")
+        continue
+    end
     n = length(PC)
     println(name, " ", n)
+    n > 0 || continue
     PH = PC2PH(PC)
     b1 = barcodes(PH,1)
     b2 = barcodes(PH,2)
-    # persistence
-    p1 = b1[:,2]-b1[:,1]
-    p2 = b2[:,2]-b2[:,1]
     r1 = representatives(PH,1)
     r2 = representatives(PH,2)
-    H1 = representatives2H(r1, n)
-    H2 = representatives2H(r2, n)
-    # only use node cent since edge cent is too similar to persistence
-    cent1 = centralities(H1; edge_weights=p1)[1]
-    cent2 = centralities(H2; edge_weights=p2)[1]
     dic = Dict(:n => n,
                :x => [p[1] for p in PC],
                :y => [p[2] for p in PC],
                :z => [p[3] for p in PC],
                :H1 => Dict(:barcode => b1, :representatives => r1),
                :H2 => Dict(:barcode => b2, :representatives => r2),
-               :cent1 => cent1,
-               :cent2 => cent2,
               )
+    # add centralities unless trivial
+    if size(b1,1) > 0 dic[:cent1] = centralities(rep2H(r1, n); edge_weights=b1[:,2]-b1[:,1]) end
+    if size(b2,1) > 0 dic[:cent2] = centralities(rep2H(r2, n); edge_weights=b2[:,2]-b2[:,1]) end
     GZip.open(outfile, "w") do io JSON.print(io, dic) end
 end
