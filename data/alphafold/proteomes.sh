6d51fca2aa0773f7f74155ac18199fc0ea86d239
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-10-20 05:49:14 +0000

    +crcmod

--- data/alphafold/proteomes.sh
+++ data/alphafold/proteomes.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env zsh
 # https://github.com/deepmind/alphafold/blob/main/afdb/README.md
+# gsutil and gcloud commands installed as part of google-cloud-cli:
+# https://cloud.google.com/storage/docs/gsutil_install#linux
+# follow link from:
+gcloud init
+# install crcmod since it is recommended for more speed
+gsutil help crcmod
+pip3 install --no-cache-dir -U crcmod
+# see that it is set to true
+gsutil version -l
 # in tmux downloaded with 
 gsutil -m cp -r gs://public-datasets-deepmind-alphafold/proteomes/ .
