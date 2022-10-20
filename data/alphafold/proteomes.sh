193db92747ac75e4903921e800fa151d457f9b6e
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-10-20 01:17:40 +0000

    +TarIterators

--- data/alphafold/proteomes.sh
+++ data/alphafold/proteomes.sh
@@ -2,10 +2,3 @@
 # https://github.com/deepmind/alphafold/blob/main/afdb/README.md
 # in tmux downloaded with 
 gsutil -m cp -r gs://public-datasets-deepmind-alphafold/proteomes/ .
-# then untared with
-\ls -f | while read file; do
-    if [[ "$file:e" == "tar" ]]; then
-        tar -xf $file && rm $file
-    fi
-done
-
--- install.jl
+++ install.jl
@@ -1,4 +1,4 @@
 #!/usr/bin/env julia
 using Pkg
 Pkg.activate(".")
-Pkg.add(["SparseArrays", "LinearAlgebra", "JSON", "GZip", "Ripserer"])
+Pkg.add(["SparseArrays", "LinearAlgebra", "JSON", "GZip", "Ripserer", "TarIterators"])
