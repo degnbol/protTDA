2f41dc46ce024c4bc37bf9d87dce16b3c57eaba1
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-11-07 17:05:39 +1100

    all PDB annotations with seqid indexing, not resi

--- data/ECOD/ECOD_bounds.tsv.sh
+++ data/ECOD/ECOD_bounds.tsv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 sed '1,4d' ecod.latest.domains.txt | sed '1s/^#//' |
-    mlr -t cut -f ecod_domain_id,manual_rep,f_id,pdb,chain,pdb_range +\
+    mlr -t cut -f ecod_domain_id,manual_rep,f_id,pdb,chain,seqid_range +\
     put '$manual = $manual_rep == "MANUAL_REP" ? 1 : 0;
     $domain = substr($ecod_domain_id, -1, -1)' +\
     rename 'pdb,PDB,f_id,ECOD' | table_unjag.sh 6 $'\t' , |
-    mlr -t put '$pdb_range =~ "^(.):(-?[0-9]+)-(-?[0-9]+)$";
+    mlr -t put '$seqid_range =~ "^(.):(-?[0-9]+)-(-?[0-9]+)$";
     $chain = "\1"; $start = "\2"; $stop = "\3"' +\
-    cut -x -f ecod_domain_id,manual_rep,pdb_range > ECOD_bounds.tsv
+    cut -x -f ecod_domain_id,manual_rep,seqid_range > ECOD_bounds.tsv
--- data/PDB_annots/RUNME.sh
+++ data/PDB_annots/RUNME.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 # to reproduce the data run this.
 # Not necessary since the data is added to git.
+# NOTE: ranges are seqid ranges, not residue ranges. So the values are relative 
+# to the first residue of the chain, not the full sequence.
 cd $0:h
 ../CATH/RUNME.sh
 ../SCOPe/RUNME.sh
