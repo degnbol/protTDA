cf66b3329f0bf139003b941a2b9cdb6080942935
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-11-07 15:49:13 +1100

    add pdb annots to main runme

--- data/PDB_annots/RUNME.sh
+++ data/PDB_annots/RUNME.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 # to reproduce the data run this.
 # Not necessary since the data is added to git.
+cd $0:h
 ../CATH/RUNME.sh
 ../SCOPe/RUNME.sh
 ../ECOD/RUNME.sh
--- data/RUNME.sh
+++ data/RUNME.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env zsh
+# Run this script to recreate data.
 $0:h/CASP14/RUNME.sh
+$0:h/PDB_annots/RUNME.sh
