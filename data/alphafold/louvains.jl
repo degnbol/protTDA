c969bd9ea607ad24f1a697c3ad7992162647b80e
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-11-10 03:56:55 +0000

    louvain

--- data/alphafold/louvains.jl
+++ data/alphafold/louvains.jl
@@ -2,18 +2,19 @@
 using Random
 ROOT = readchomp(`git root`)
 
-omes = vcat([readdir(d1) for d1 in readdir("PH"; join=true)]...)
+omes = vcat([readdir(d1; join=true) for d1 in readdir("PH"; join=true)]...)
 println(length(omes), " proteomes")
 
-omes = omes[.!isfile.(omes .* "/.inprogress")]
+omes = omes[.!isfile.(omes .* "/louvain.json.gz")]
 println(length(omes), " todo")
 
 shuffle!(omes)
 
-for outdir in omes[1:min(1000,length(todo))]
+for outdir in omes[1:min(1000,length(omes))]
+    isfile(outdir*"/.inprogress") && continue
     touch(outdir*"/.inprogress")
-    println(proteome)
-    res = run(`$ROOT/src/louvain.py $outdir $outdir/louvain.json`)
+    println(outdir)
+    res = run(`$ROOT/src/louvain.py $outdir/'AF*.json.gz' $outdir/louvain.json.gz`)
     res.exitcode == 0 && rm(outdir*"/.inprogress")
 end
 
