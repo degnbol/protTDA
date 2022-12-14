b4c6d7b83c2143742f47da1cd34a0aecadfa08ec
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-12-14 13:27:26 +1100

    tempura

---
+++ data/TEMPURA/200617_TEMPURA.csv.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+# paper: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7511790/
+# doi: https://doi.org/10.1264%2Fjsme2.ME20074
+# DB: http://togodb.org/db/tempura
+wget http://togodb.org/release/200617_TEMPURA.csv
