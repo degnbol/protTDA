b9c3440e4b70f7ce9d11d55821acb35e8b7ea5ee
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-01-09 04:40:17 +0000

    sep mlr and init call

--- data/RCSB/PDB2acc.tsv.gz.sh
+++ data/RCSB/PDB2acc.tsv.gz.sh
@@ -4,6 +4,6 @@ WORK=`git root`/data/alphafold/dl/RCSB/
 # there is an entry _struct_ref.pdbx_db_accession which is followed by 
 # whitespace, then the uniprot accession, a blank or the PDB id then a single 
 # whitespace.
-gunzip -cr $WORK/mmCIF | grep -E '^data_|^_struct_ref.pdbx_db_accession' | sed 's/data_/\nPDB /' |
-    mlr --x2t clean-whitespace + rename '_struct_ref.pdbx_db_accession,accession' +\
-    filter '$accession != "" && $accession != $PDB' > PDB2acc.tsv
+gunzip -cr $WORK/mmCIF | grep -E '^data_|^_struct_ref.pdbx_db_accession' | sed 's/data_/\nPDB /' > PDB2acc.tmp.tsv
+mlr --x2t --from PDB2acc.tmp.tsv clean-whitespace + rename '_struct_ref.pdbx_db_accession,accession' +\
+    filter '$accession != "" && $accession != $PDB' | gzip > PDB2acc.tsv.gz
