aeb0670bac8c1c2810bfd351345e3e54576c6072
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-01-23 02:42:00 +0000

    moved home to neo4j/neo4j/

--- neo4j/env.sh
+++ neo4j/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-export NEO4J_HOME=$HOME/protTDA/neo4j
-export NEO4J_CONF=$NEO4J_HOME/conf
+export NEO4J_HOME=$HOME/protTDA/neo4j/neo4j/
+export NEO4J_CONF=$HOME/protTDA/neo4j/conf/
 export NEO4J_USERNAME=neo4j
 export NEO4J_PASSWORD=cdmadsen
--- neo4j/install.sh
+++ neo4j/install.sh
@@ -2,18 +2,24 @@
 # neo4j was installed on oracle cloud like this.
 sudo rpm --import https://debian.neo4j.com/neotechnology.gpg.key
 sudo cp neo4j.repo /etc/yum.repos.d/
-NEO4J_ACCEPT_LICENSE_AGREEMENT=yes sudo yum install neo4j-enterprise-5.3.0
+sudo NEO4J_ACCEPT_LICENSE_AGREEMENT=yes yum install neo4j-enterprise-5.3.0
 
-cp -r /var/lib/neo4j/labs/ .
-cp -r /var/lib/neo4j/products/ .
-mv labs/apoc*.jar plugins/ # as per labs/README.txt
+ln -s ../data/alphafold/PH/neo4j/ neo4j
+
+# shows that license has been accepted:
+cp -r /var/lib/neo4j/licenses/ neo4j/
+cp -r /var/lib/neo4j/data neo4j/
+cp -r /var/lib/neo4j/labs/ neo4j/
+cp -r /var/lib/neo4j/products/ neo4j/
+mkdir -p neo4j/plugins/
+cp neo4j/labs/apoc*.jar neo4j/plugins/ # as per labs/README.txt
 
 # for apoc.load.directory
 apocExtUrl='https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/5.3.0/apoc-5.3.0-extended.jar'
-wget $apocExtUrl -O plugins/apoc-5.3.0-extended.jar
-
+wget $apocExtUrl -O neo4j/plugins/apoc-5.3.0-extended.jar
 
 ROOT=`git root`
-NEO4J_HOME=$ROOT/neo4j NEO4J_CONF=$NEO4J_HOME/conf neo4j-admin dbms set-initial-password cdmadsen
+NEO4J_HOME=$ROOT/neo4j/neo4j/ NEO4J_CONF=$ROOT/neo4j/conf neo4j-admin dbms set-initial-password cdmadsen
 
 echo 'source $HOME/protTDA/neo4j/env.sh' >> ~/.zshrc
+
