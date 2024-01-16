1190b7f779128a17d25a6f571873bce0e98e7ba9
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-01-16 17:43:31 +1100

    wrong wget flag

--- data/homolog/russel1998/cifs.sh
+++ data/homolog/russel1998/cifs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 mkdir -p cifs/
-cat pdbs.txt | xargs -I {} wget -O cifs/ 'https://files.rcsb.org/download/'{}'.cif.gz'
+cat pdbs.txt | xargs -I {} wget -P cifs/ 'https://files.rcsb.org/download/'{}'.cif.gz'
 
