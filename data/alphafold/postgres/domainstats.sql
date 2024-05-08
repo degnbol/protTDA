4d8debd8cd4a6fd10913e768f32383f21bafa5ca
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-05-08 19:45:51 +1000

    model organism protein hists

--- data/alphafold/postgres/domainstats.sql
+++ data/alphafold/postgres/domainstats.sql
@@ -37,9 +37,7 @@ FROM speciespern
 GROUP BY domain;
 
 -- we can see avg of avg (species weighted) fits what we have seen before
--- DROP TABLE speciespern;
-    
--- \copy pern to 'pern' csv;
+DROP TABLE speciespern;
 
 create table taxweight as
 select tax, 1::numeric/count(*) as weight from pern
@@ -116,8 +114,93 @@ CREATE table domainhist AS
   UNION ALL
   SELECT * FROM maxrep2_hist;
 
-drop table (nrep1_hist, nrep2_hist, maxrep1_hist, maxrep2_hist);
+drop table nrep1_hist, nrep2_hist, maxrep1_hist, maxrep2_hist;
 
 \copy domainhist to 'domainhist.csv' csv header;
 \copy hist_maxs to 'domainhistmaxs.csv' csv header;
 
+create table models (
+    label TEXT NOT NULL,
+    tax INTEGER PRIMARY KEY
+);
+
+\copy models from 'vis/modelOrganisms.tsv' DELIMITER E'\t' CSV HEADER;
+
+CREATE TABLE pern_models AS
+SELECT models.label, pern.*
+FROM models INNER JOIN pern ON models.tax = pern.tax;
+
+create table nrep1_hist as
+select width_bucket(nrep1, 0, 3.7142857142857143, 1000) as bucket,
+    label,
+    pern_models.tax,
+    domain,
+    count(*) as freq,
+    sum(weight) as weighted
+from pern_models inner join taxweight on pern_models.tax = taxweight.tax
+group by bucket, domain, pern_models.tax, label
+order by bucket;
+
+create table nrep2_hist as
+select width_bucket(nrep2, 0, 2.9665178571428571, 1000) as bucket,
+    label,
+    pern_models.tax,
+    domain,
+    count(*) as freq,
+    sum(weight) as weighted
+from pern_models inner join taxweight on pern_models.tax = taxweight.tax
+group by bucket, domain, pern_models.tax, label
+order by bucket;
+
+create table maxrep1_hist as
+select width_bucket(maxrep1, 0, 1.0089285714285714, 1000) as bucket,
+    label,
+    pern_models.tax,
+    domain,
+    count(*) as freq,
+    sum(weight) as weighted
+from pern_models inner join taxweight on pern_models.tax = taxweight.tax
+group by bucket, domain, pern_models.tax, label
+order by bucket;
+
+create table maxrep2_hist as
+select width_bucket(maxrep2, 0, 1.4285714285714286, 1000) as bucket,
+    label,
+    pern_models.tax,
+    domain,
+    count(*) as freq,
+    sum(weight) as weighted
+from pern_models inner join taxweight on pern_models.tax = taxweight.tax
+group by bucket, domain, pern_models.tax, label
+order by bucket;
+
+alter table nrep1_hist   add H INTEGER;
+alter table nrep2_hist   add H INTEGER;
+alter table maxrep1_hist add H INTEGER;
+alter table maxrep2_hist add H INTEGER;
+alter table nrep1_hist   add meas VARCHAR(6);
+alter table nrep2_hist   add meas VARCHAR(6);
+alter table maxrep1_hist add meas VARCHAR(6);
+alter table maxrep2_hist add meas VARCHAR(6);
+
+update nrep1_hist   set H=1;
+update nrep2_hist   set H=2;
+update maxrep1_hist set H=1;
+update maxrep2_hist set H=2;
+update nrep1_hist   set meas='nrep';
+update nrep2_hist   set meas='nrep';
+update maxrep1_hist set meas='maxrep';
+update maxrep2_hist set meas='maxrep';
+
+CREATE table modelshist AS
+  SELECT * FROM nrep1_hist
+  UNION ALL
+  SELECT * FROM nrep2_hist
+  UNION ALL
+  SELECT * FROM maxrep1_hist
+  UNION ALL
+  SELECT * FROM maxrep2_hist;
+
+drop table nrep1_hist, nrep2_hist, maxrep1_hist, maxrep2_hist;
+
+\copy modelshist to 'modelshist.csv' csv header;
