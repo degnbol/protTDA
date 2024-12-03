b8afe5e9b66ce8a3f0a168af850ffa6e09f96a75
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-12-03 21:35:10 +1100

    comment

--- data/mut/P30566_AAs.sh
+++ data/mut/P30566_AAs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 cd $0:h
+
+# uniprot accession found by finding the single human entry for Adenylosuccinate lyase.
+
 echo "Neutral"
 mlr --c2t --from mutations.csv.gz filter '$ID == "P30566" && $Label == "Neutral and Structurally Neutral"' + uniq -f AA | sed 1d | sort -n
 echo "Damaging"
