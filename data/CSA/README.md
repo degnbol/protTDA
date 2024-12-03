abb7037c25c32364e558009f5fb30b1f27b2af56
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-12-03 17:17:00 +1100

    mutations

---
+++ data/CSA/README.md
@@ -0,0 +1,7 @@
+M-CSA has >4000 residues marked within catalytic sites of <1000 proteins.
+They're indicated with PDB id.
+Agnese mapped them to uniprot entries, which means we have the AF entries as they follow the uniprot naming.
+Then the AF protein topology outputs were collected and the plots were made in notebook and other data available on mediaflux.
+Here, I ran PH directly on the experimental structures.
+The thing to gain from this is another validation of the applicability of AF.
+
---
+++ data/mut/muts.pdf.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(ggplot2))
+
+dt = fread("./mutations.csv.gz", select=c(
+    "Type",
+    "Structural prediction",
+    "confidence",
+    "Centrality degree 1",
+    "Centrality degree 2"
+))
+dt = dt[confidence == "high"]
+dt[,confidence:=NULL]
+setnames(dt, "Type", "Disease")
+setnames(dt, "Structural prediction", "Structural")
+setnames(dt, "Centrality degree 1", "1D cycles")
+setnames(dt, "Centrality degree 2", "2D cycles")
+
+dtm = melt(dt, c("Disease", "Structural"), variable.name="Dimension", value.name="TIF")
+dtm[Disease=="Disease", Disease:="Damaging"]
+dtm[Structural=="Disease", Structural:="Damaging"]
+dtm[, Disease:=factor(Disease, levels=c("Neutral", "Damaging"))]
+dtm[, Structural:=factor(Structural, levels=c("Neutral", "Damaging"))]
+
+ggplot(dtm, aes(Structural, TIF, color=Disease, fill=Structural)) +
+    facet_grid(cols=vars(Dimension)) +
+    geom_boxplot(notch=TRUE, linewidth=0.9) +
+    theme_bw() +
+    scale_fill_manual(values=c("white", "#e68e87")) +
+    scale_color_manual(values=c("#2f2f2f", "#c20d00")) +
+    theme(
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.x=element_blank(),
+        axis.ticks.x=element_blank()
+    )
+
+ggsave("muts.pdf", width=5.55, height=4.64)
+
